abb75af3da44c5a11061b65bfef19d3d43d3384a
Rosk <vape00@hotmail.com>
2018-09-04 18:25:22 -0700

    Incorporating Miscellany

---
+++ Miscellany/Centering/Centering/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----X----");
+            Console.WriteLine(String.Format("i{0,4}i", "^"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Miscellany/Sidebar/Sidebar/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sidebar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.BackgroundImage = Properties.Resources.KayleeBG;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.BackgroundImage = Properties.Resources.KayleeBG2;
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.BackgroundImage = Properties.Resources.KayleeBG3;
+        }
+
+        private void toolStripContainer1_TopToolStripPanel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
